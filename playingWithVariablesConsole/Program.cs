6882f0b47475144b8e4f135216c578ae46ba264e
Peter luque <Peterluque21@gmail.com>
2022-08-26 16:23:42 -0400

    initial commit

---
+++ playingWithVariablesConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information             
+// Variable Declartions
+int age;
+string name;
+bool isAlive;
+
+age = 22;
+
+name = "Penny";
+
+isAlive = true;
+
+string fullName = "Penny Luque";
+int houseNumber = 8602;
+
+Console.WriteLine($"{name} {age} is he alive: {isAlive}");
